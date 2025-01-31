2d91a65895e7dc1b42aa2fb96c4ef1e0dbf57468
trevortomesh <tmtomesh@hotmail.com>
2025-01-31 16:13:27 -0600

    uploading Main.java, etc.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x; // this is an integer
+        x = 5;
+        String s;
+        boolean b;
+        b = false;
+        s = "I like eggs";
+        float float_pi = 3.14f;
+        double pi = 3.14;
+        int r = 1;
+        double area = pi * Math.pow(r, 2);
+        System.out.println("The area is " + area);
+        System.out.print("Hello, World!");
+        System.out.println("I'm Dr. T!");
+        System.out.println("I like cats!" + " and " + s);
+        System.out.println(x);
+        System.out.println(11 % 10);
+
+        int a = 4;
+        int c = 2;
+        System.out.println(a/c);
+
+    }
+}
